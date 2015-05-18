71545dc9693377b078e6645e66bd0791294ccc44
joykunnia <joykunnia@gmail.com>
2015-05-18 20:37:13 +0900

    테스트 케이스 추가 및 약간 리펙토링

--- lists/tests.py
+++ lists/tests.py
@@ -25,6 +25,12 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = '신규 작업 아이템'
 
         response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+
         self.assertIn('신규 작업 아이템', response.content.decode())
         
         expected_html = render_to_string(
@@ -32,6 +38,7 @@ class HomePageTest(TestCase):
             {'new_item_text': '신규 작업 아이템'}
         )
         self.assertEqual(response.content.decode(), expected_html) 
+
         
 class ItemModelTest(TestCase):
 
